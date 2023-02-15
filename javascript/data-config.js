fc78c2c31282caefa229c436356dc69ce9dd2d2c
MapleAtMorning <79129529+MapleAtMorning@users.noreply.github.com>
2023-02-14 23:13:41 -0600

    Upgrades Management
    More store updates, mostly to add the disabled functionality to certain upgrades that shouldn't be unlocked yet.

--- javascript/data-config.js
+++ javascript/data-config.js
@@ -1,5 +1,7 @@
 let bytes = 0;
+let upgrades = [0, 0, 0]; // Loops, Keyboard, Monkey
 let bytesText = document.getElementById("bytesNum");
+
 
 export function addBytes(amount){
     if(!isNaN(amount) && amount >= 1){
@@ -10,12 +12,41 @@ export function addBytes(amount){
 };
 
 export function removeBytes(amount){
-    if(!isNaN(amount) && bytes > amount){
+    if(!isNaN(amount) && bytes >= amount){
         bytes -= amount;
         updateBytes();
         return(bytes);
     };
 };
+
+export function upgradeManager(cond, upgrade){ // Pass in the condition such as add or subtract (for some reason), or even check. Then comma and put in what element in the array you're checking.
+    if(cond == "add"){
+        upgrades[upgrade]++;
+
+    }if(cond == "sub"){
+        upgrades[upgrade]--;
+        
+    }else if(cond == "check"){
+        if(!isNaN(upgrade)){
+            return(upgrades[upgrade]);
+        }
+
+    }else{
+        return;
+    }
+}
+
+export function bytesCheck(){
+    return(bytes);
+}
+
+function loadData(){
+
+}
+
+function saveData(){
+
+}
 
 function updateBytes(){
     bytesText.innerHTML = bytes;
--- javascript/gameplay.js
+++ javascript/gameplay.js
@@ -1,8 +1,33 @@
-import { addBytes, removeBytes } from "/javascript/data-config.js";
+import { addBytes, removeBytes, bytesCheck, upgradeManager} from "/javascript/data-config.js";
 
 let hackButton = document.getElementById("clicker");
 
 
+
 hackButton.addEventListener("click", function(){
     addBytes(1)
+});
+
+document.addEventListener('click', function (event) {
+    let clickTarget = event.target;
+    let bytes = bytesCheck();
+
+	if (clickTarget.matches('.grid-item') && !clickTarget.matches('.disabled')) {
+        let price = parseInt(clickTarget.getElementsByClassName('price')[0].innerHTML); // Get the number from the price class in the grid item you clicked
+
+        if(bytes < price){ // Simple bytes check to see if you have enough to buy the upgrade.
+            console.log("Not enough money");
+            return;
+        }
+        removeBytes(price);
+
+        if(clickTarget == document.getElementById('loops')){
+            upgradeManager("add", 0);
+                        
+            if(upgradeManager("check", 0) == 1){
+                document.getElementById("keyboard").classList.remove("disabled");
+            }         
+        }
+	}
+
 });
