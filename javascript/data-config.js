81676c1ff99dcf7d3636a690ae01279c84836388
MapleAtMorning <79129529+MapleAtMorning@users.noreply.github.com>
2023-02-15 09:55:21 -0600

    More Shop
    Updated disabled and made click-ability more consistent. Most of the shop was also changed for functionality.

--- javascript/data-config.js
+++ javascript/data-config.js
@@ -1,22 +1,22 @@
 let bytes = 0;
 let upgrades = [0, 0, 0]; // Loops, Keyboard, Monkey
+let upgradesPrice = [100, 200, 300]
+let upgradesData = [10, 1, 1]
 let bytesText = document.getElementById("bytesNum");
 
 
-export function addBytes(amount){
-    if(!isNaN(amount) && amount >= 1){
-        bytes += amount;
-        updateBytes();
-        // return(bytes);
-    };
-};
-
-export function removeBytes(amount){
-    if(!isNaN(amount) && bytes >= amount){
-        bytes -= amount;
-        updateBytes();
-        return(bytes);
-    };
+export function bytesManager(cond, amount){
+    if(cond == "add"){
+        if(!isNaN(amount) && amount >= 1){
+            bytes += amount;;
+        };
+    }else if(cond == "sub"){
+        if(!isNaN(amount) && bytes >= amount){
+            bytes -= amount;
+        };
+    }
+    bytesText.innerHTML = bytes;
+    return bytes;
 };
 
 export function upgradeManager(cond, upgrade){ // Pass in the condition such as add or subtract (for some reason), or even check. Then comma and put in what element in the array you're checking.
@@ -28,26 +28,17 @@ export function upgradeManager(cond, upgrade){ // Pass in the condition such as 
         
     }else if(cond == "check"){
         if(!isNaN(upgrade)){
-            return(upgrades[upgrade]);
-        }
-
+            return upgrades[upgrade];
+        };
+    }else if(cond == "price"){
+        return upgradesPrice[upgrade];
+    }else if(cond == "value"){
+        return upgradesData[upgrade];
     }else{
         return;
-    }
+    };
 }
 
 export function bytesCheck(){
-    return(bytes);
-}
-
-function loadData(){
-
-}
-
-function saveData(){
-
-}
-
-function updateBytes(){
-    bytesText.innerHTML = bytes;
+    return bytes;
 }
--- javascript/gameplay.js
+++ javascript/gameplay.js
@@ -1,33 +1,52 @@
-import { addBytes, removeBytes, bytesCheck, upgradeManager} from "/javascript/data-config.js";
+import { bytesManager, bytesCheck, upgradeManager} from "/javascript/data-config.js";
 
 let hackButton = document.getElementById("clicker");
 
-
+function cacheLoop(){
+    setTimeout(() => {
+        console.log("Looped")
+        cacheLoop()
+    }, (upgradeManager("value", 0) * 1000));
+}
+cacheLoop()
 
 hackButton.addEventListener("click", function(){
-    addBytes(1)
+    bytesManager("add", upgradeManager("check", 2)+1);
 });
 
-document.addEventListener('click', function (event) {
+document.addEventListener('click', function(event) {
     let clickTarget = event.target;
-    let bytes = bytesCheck();
+    let bytes = bytesCheck('bytes');
 
 	if (clickTarget.matches('.grid-item') && !clickTarget.matches('.disabled')) {
+        let arrayIndex;
         let price = parseInt(clickTarget.getElementsByClassName('price')[0].innerHTML); // Get the number from the price class in the grid item you clicked
+
+        if(clickTarget == document.getElementById('loops')){                     
+            arrayIndex = 0;
+        }else if(clickTarget == document.getElementById('keyboard')){
+            arrayIndex = 1;
+        }else if(clickTarget == document.getElementById('monkey')){
+            arrayIndex = 2;
+        }else{
+            alert("You somehow clicked an upgrade that doesn't exist");
+            return;
+        }
 
         if(bytes < price){ // Simple bytes check to see if you have enough to buy the upgrade.
             console.log("Not enough money");
             return;
         }
-        removeBytes(price);
 
-        if(clickTarget == document.getElementById('loops')){
-            upgradeManager("add", 0);
-                        
-            if(upgradeManager("check", 0) == 1){
-                document.getElementById("keyboard").classList.remove("disabled");
-            }         
+        upgradeManager("add", arrayIndex)
+
+        if(upgradeManager("check", 0) == 1){
+            document.getElementById("keyboard").classList.remove("disabled");
+            document.getElementById("stats-rate").classList.remove("disabled");
+            document.getElementById("stats-amount").classList.remove("disabled");
         }
+
+        bytesManager("sub", price); // Money removal is down here so if a nonfunctional upgrade is clicked somehow money isn't taken.
 	}
 
 });
